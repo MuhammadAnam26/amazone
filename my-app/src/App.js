d2e94d4b4fcd0d5e7ea911fe74b1676f27d6ec9d
Muhammad Anam26 <115659537+MuhammadAnam26@users.noreply.github.com>
2022-10-30 10:11:07 -0700

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,58 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Route,Routes, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 import Checkout from "./Checkout";
-// import Login from "./Login"
+import Login from "./Login";
+import { auth } from "./firebase";
+import {useStateValue} from "./StateProvider";
+import {useEffect, useState} from 'react';
+
 function App() {
-return (
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+ 
+
+  return (
     <Router>
-  <div className="app">
-        <Header/>
-      {/* <Routes> */}
-        {/* <Route path="/Checkout"> */}
-        {/* <Route path="/" element={<Checkout />}> */}
-       {/* <h1>Login Page</h1> */}
-       <Checkout />
-        {/* </Route> */}
-        {/* <Route path ="/"> */}
-          <Header/>
-          <Home />
-        {/* </Route> */}
-      {/* </Routes> */}
-  </div>
-  </Router> 
-);
+        <Header />
+      <Login />
+      <div className="app">
+        {/* <Route path ="/login" element={<Login />} > 
+        </Route>  */}
+        {/* <Route path="//ogin">
+          
+
+        </Route> */}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 export default App;
