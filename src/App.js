62ef432514e35865c181af0b89c0e4db14fffc10
Muhammad Anam26 <115659537+MuhammadAnam26@users.noreply.github.com>
2022-11-03 22:08:43 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
-import {useStateValue} from "./StateProvider";
-import { useEffect } from 'react'
-import { FBAuth } from "./firebase"
+const promise = loadStripe("pk_test_51LzOeeIXVxSYRSKMTgehzyeXbuVa2DdkvKPVejZVxDRXkEo22E3Lk54fddNnBSnr6TCDuZUgatO60pRXIEYucpOs00pXVbnQy4");
 
 function App() {
-  const [dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
+
   useEffect(() => {
     // will only run once when the app component loads...
 
-    FBAuth.onAuthStateChanged((authUser) => {
+    auth.onAuthStateChanged((authUser) => {
       console.log("THE USER IS >>> ", authUser);
 
       if (authUser) {
@@ -33,20 +38,25 @@ function App() {
         });
       }
     });
-  }, [dispatch]);
- 
+  }, []);
 
   return (
     <Router>
-        <Header />
-      <Login />
       <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
+        {/* In react-router-dom v6, 'Routes' is used instead of Switch */}
+        <Routes>  
+              <Route path="/orders" element={[<Header />, <Orders />]}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/" element={[<Header />, <Home />]}/>
+              <Route path="/checkout" element={[<Header />, <Checkout />]}/>
+              <Route path="/payment" element={[<Header />, 
+              <Elements stripe={promise} >
+                <Payment /> 
+              </Elements> ]}/>
         </Routes>
       </div>
     </Router>
   );
 }
+
 export default App;
